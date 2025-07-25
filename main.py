66ea90aa8572e6a2debaa919bb6858e90048f60a
davidsteimel <david.steimel02@gmail.com>
2025-07-25 15:19:26 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.data.loader import load_match_data
+
+wm_data = load_match_data()
+
+row_count, column_count = wm_data.shape
+
+nan_count = wm_data.isna().sum()
+
+print(f"Die Daten enthalten {row_count} Zeilen und {column_count} Spalten.")
+print(f"Die Daten enthalten {nan_count} fehlende Werte.")
